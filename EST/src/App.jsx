5b408932f6377cdf3bc4eadcfe8652b3496ff4ec
vishwizard <deyamrit959@gmail.com>
2025-11-15 14:25:15 +0530

    Done

---
+++ EST/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Name from './Name'
+
+function App() {
+
+  return (
+    <>
+      <Name name="Vishal"></Name>
+      <Name></Name>
+    </>
+  )
+}
+
+export default App
+
---
+++ EST/src/Name.jsx
@@ -0,0 +1,5 @@
+export default function Name  ({name = "Geust"}){
+    return (<>
+        <h1>Welcome {name}!</h1>
+    </>);
+};
